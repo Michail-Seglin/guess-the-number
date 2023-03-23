d2ade099338442a80c8e9f43186e8ddc3f9f35ce
Michail-Seglin <mihailsavuskin13@gmail.com>
2023-03-23 16:06:29 +0300

    1

--- app.js
+++ app.js
@@ -1,14 +1,43 @@
-// Работа с замыканием. Напишите функцию, каждый вызов который будет
-// генерировать случайные числа от 1 до 100
+let rNum = Math.round(Math.random() * 100);
 
-function randomNum() {
-    return function () {
-        console.log(Math.floor(Math.random() * 100));
+function guessTheNumber(rNum_) {
+    try {
+        let stNum = 0;
+        let fNum = 100;
+
+        for (let i = 0; i < fNum; i++) {
+            let iNum = prompt(`Введите число от ${stNum} до ${fNum}`
+            );
+            if (iNum === null) {
+                alert('Вы не угадали');
+                break;
+            }
+
+            if (iNum == rNum_) {
+                alert("Вы угадали!");
+                break;
+            } else if (iNum > rNum_) {
+                if (fNum > iNum) {
+                    fNum = iNum;
+                }
+
+                alert(
+                    `Вы ввели число ${iNum} и это слишком большое значение. Введите число от ${stNum} до ${fNum}`
+                );
+                continue;
+            } else if (iNum < rNum_) {
+                if (stNum < iNum) {
+                    stNum = iNum;
+                }
+                alert(
+                    `Вы ввели число ${iNum} и это слишком маленькое значение. Введите число от ${stNum} до ${fNum}`
+                );
+                continue;
+            }
+        }
+    } catch (er) {
+        return er.message;
     }
 }
-const res = randomNum();
-res()
-res()
-res()
-res()
-res()
+
+guessTheNumber(rNum);
