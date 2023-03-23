05cf8811bf2eb7b78fe4075e5e7fe702b94a4076
Michail-Seglin <mihailsavuskin13@gmail.com>
2023-03-23 17:08:01 +0300

    OOP

--- app.js
+++ app.js
@@ -1,43 +1,47 @@
-let rNum = Math.round(Math.random() * 100);
+class Num {
 
-function guessTheNumber(rNum_) {
-    try {
-        let stNum = 0;
-        let fNum = 100;
+    guessTheNumber(rNum_) {
+        try {
+            let stNum = 0;
+            let fNum = 100;
 
-        for (let i = 0; i < fNum; i++) {
-            let iNum = prompt(`Введите число от ${stNum} до ${fNum}`
-            );
-            if (iNum === null) {
-                alert('Вы не угадали');
-                break;
-            }
-
-            if (iNum == rNum_) {
-                alert("Вы угадали!");
-                break;
-            } else if (iNum > rNum_) {
-                if (fNum > iNum) {
-                    fNum = iNum;
+            for (let i = 0; i < fNum; i++) {
+                let iNum = prompt(`Введите число от ${stNum} до ${fNum}`
+                );
+                if (iNum === null) {
+                    alert('Вы не угадали');
+                    break;
                 }
 
-                alert(
-                    `Вы ввели число ${iNum} и это слишком большое значение. Введите число от ${stNum} до ${fNum}`
-                );
-                continue;
-            } else if (iNum < rNum_) {
-                if (stNum < iNum) {
-                    stNum = iNum;
+                if (iNum == rNum_) {
+                    alert("Вы угадали!");
+                    break;
+                } else if (iNum > rNum_) {
+                    if (fNum > iNum) {
+                        fNum = iNum;
+                    }
+
+                    alert(
+                        `Вы ввели число ${iNum} и это слишком большое значение. Введите число от ${stNum} до ${fNum}`
+                    );
+                    continue;
+                } else if (iNum < rNum_) {
+                    if (stNum < iNum) {
+                        stNum = iNum;
+                    }
+                    alert(
+                        `Вы ввели число ${iNum} и это слишком маленькое значение. Введите число от ${stNum} до ${fNum}`
+                    );
+                    continue;
                 }
-                alert(
-                    `Вы ввели число ${iNum} и это слишком маленькое значение. Введите число от ${stNum} до ${fNum}`
-                );
-                continue;
             }
+        } catch (er) {
+            return er.message;
         }
-    } catch (er) {
-        return er.message;
     }
+
 }
 
-guessTheNumber(rNum);
+let rNum = Math.round(Math.random() * 100);
+const num = new Num();
+num.guessTheNumber(rNum);
